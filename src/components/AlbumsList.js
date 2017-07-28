d2eb6912689e43a1f38dc6682f4493f8d3508dae
Weslley Rocha <weslleytato.12@gmail.com>
2017-07-28 10:30:16 -0300

    card

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumsDetail from './AlbumDetail';
 
 class AlbumsList extends Component {
-	state = { albums: [] }
+  state = { albums: [] }
 
   componentWillMount() {
-		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-			.then(response => this.setState({ albums: response.data }));
-	}
-		
-	renderAlbums() {
-		return this.state.albums.map(album => <Text>{album.title}</Text>);
-	}
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumsDetail key={album.title} album={album} />);
+  }
 
   render() {
-		console.log(this.state);
+    console.log(this.state);
     return (
       <View>
-        {this.renderAlbums()} 
+        {this.renderAlbums()}
       </View>
     );
   }
